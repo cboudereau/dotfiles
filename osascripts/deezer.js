4cc1bc47e5efe6a104286bdd39d9d7d3f9b06cb9
cboudereau <clement.boudereau@zen.ly>
2021-10-09 15:13:02 +0200

    deezer

--- osascripts/deezer.js
+++ osascripts/deezer.js
@@ -1,6 +1,9 @@
-let output = "";
+let output = "Why aren't you listening to music with deezer :) ?";
 let app = Application("Google Chrome");
 if (app.running()){
-  output=[...new Set(app.windows().flatMap(x=>x.tabs()).filter(x=>x.url().includes("deezer.com")).map(x=>x.title()))];
+  let result=[...new Set(app.windows().flatMap(x=>x.tabs()).filter(x=>x.url().includes("deezer.com")).map(x=>x.title()))];
+  if(result.length > 0) {
+    output=result;
+  }
 }
 output;
