69c9e78e98ac773fc4ae6b3323f390ba1b813f43
Clément Boudereau <cboudereau@gmail.com>
2025-04-03 11:32:03 +0200

    feat: add powershell aliases

--- wsl2/Microsoft.PowerShell_profile.ps1
+++ wsl2/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,23 @@ Set-Alias -Name d -Value docker
 Set-Alias -Name dc -Value 'docker-compose'
 Set-Alias -Name ca -Value cargo
 
+function dcd() {
+    docker-compose down --remove-orphans -v --rmi local
+}
+
+function dcu() {
+    docker-compose down --remove-orphans -v --rmi local
+    docker-compose up
+}
+
+function ddf () {
+    docker system df
+}
+
+function dsp() {
+    docker system prune -a -f --volumes
+}
+
 function dotenv() {
     get-content .env | foreach {
         $name, $value = $_.split('=')
